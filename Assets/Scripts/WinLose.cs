c2e9f9a12d3472026a2cf62fc4a4a27c0d4c3f61
Jafar Abdulrasoul <justjimmar@gmail.com>
2016-12-04 16:36:53 -0800

    Calls win/lose audio clip when winning/losing

--- Assets/Scripts/WinLose.cs
+++ Assets/Scripts/WinLose.cs
@@ -8,14 +8,29 @@ public class WinLose : MonoBehaviour {
 	// Use this for initialization
 	[SerializeField] Texture2D win;
 	[SerializeField] Texture2D lose;
+	[SerializeField] AudioClip winClip;
+	[SerializeField] AudioClip loseClip;
+	AudioSource audioSource;
 
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update(){
+		
+	}
 	public void Win(){
 		GetComponent<RawImage>().texture = win;
 		GetComponent<Animation>().Play();
+		audioSource.clip = winClip;
+		audioSource.Play();
+
 	}
 
 	public void Lose(){
 		GetComponent<RawImage>().texture = lose;
 		GetComponent<Animation>().Play();
+		audioSource.clip = loseClip;
+		audioSource.Play();
 	}
 }
